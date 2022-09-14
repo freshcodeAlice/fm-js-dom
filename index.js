d5af1ef0f12f52f725affedb92e75eaddc89e034
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-14 11:37:37 +0300

    Img src task resolve

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 /*
-1. Маємо div
-2. Маємо 5 кнопок, на кожній написаний колір
-3. За натиснення на кнопку фоновий колір оцього діва змінюється на відповідний колір, вказаний на кнопці
+1. Маємо елемент img
+2. Маємо дві кнопки, які в атрибутах data- містять посилання на певну картинку (адреса картинки)
+3. За натиснення на кнопку img має показати ту картинку, адреса якої зберігалася у кнопці.
+
+
 
 */
 
+const [btn1, btn2] = document.querySelectorAll('button');
 
-const buttons = document.querySelectorAll('button');
-// const div = document.querySelector('#root');
+btn1.addEventListener('click', clickHandler);
+btn2.addEventListener('click', clickHandler);
 
-for (const btn of buttons) {
-    btn.addEventListener('click', clickHandler);
-}
-
-
-function clickHandler ({target: {dataset: {color}, parentNode}}) {
-    parentNode.style.backgroundColor = color;
+function clickHandler ({target}) {
+    const img = target.parentNode.children[0];
+    img.setAttribute('src', target.dataset.src);
 }
