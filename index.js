66bbff0e80c896f0b76b8655f6d261e246daca9f
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-14 09:28:50 +0300

    Bubbling and capturing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 
 */
-
+/*
 const div = document.querySelector('.box');
 const classList = div.classList;
 const [red, green] = document.querySelectorAll('.btn');
@@ -17,3 +17,33 @@ red.addEventListener('click', () => {
 green.addEventListener('click', () => {
     classList.toggle('green-big');
 })
+
+
+
+ */
+
+
+const btn = document.querySelector('button');
+
+const eventHandler = (event) => {
+    console.dir(event.currentTarget); //той, кому належить eventListener
+    console.dir(event.target); // той, на кому спрацювала подія
+}
+
+window.addEventListener('click', eventHandler);
+
+/*
+3 фази події:
+
+1. Фаза занурення. Подія стається на рівні ОС, ОС передає її браузеру (Window), той передає document -> body -> ..... -> елемент, на якому сталася подія
+
+2. Фаза цілі. Подія досягла елемента, елемент - це таргет
+
+3. Фаза сплиття. Подія починає спливати у зворотньому напрямку, тобто від елемента до браузера.
+
+
+*/
+
+
+// const event = new MouseEvent('click');
+// btn.dispatchEvent(event);
