6afd4b750cc25c3d323ec1836f70adfac753f860
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-15 11:03:37 +0300

    creating image

--- index.js
+++ index.js
@@ -2,4 +2,18 @@
 const div = document.createElement('div'); 
 // 2. Приєднати новий елемент до будь-якого існуючого на сторінці батьківського елемента
 const body = document.body;
+div.append('Hello, JS');
+div.append('Another text');
 body.append(div);
+
+
+/*
+Створити картинку і вставити її в div
+
+*/
+
+
+const img = document.createElement('img');
+img.setAttribute('src', './images/pict.jpg');
+img.classList.add('picture');
+div.append(img);
