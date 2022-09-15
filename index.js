4ac26f7b03cf2c5d39bc0f7addd9b5eb41418080
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-15 12:02:56 +0300

    createUserCard (need refactor)

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-// 1. Створити новий елемент
-const div = document.createElement('div'); 
-// 2. Приєднати новий елемент до будь-якого існуючого на сторінці батьківського елемента
-const body = document.body;
-div.append('Hello, JS');
-div.append('Another text');
-body.append(div);
+const root = document.querySelector('#root');
+
+const cardArray = data.map((user)=>createCard(user));
+
+function createCard(user) {
+    /* TODO:
+    1. Refactor to short code
+    2. Create image placeholder: first letter of name + random background-color
+    3. When image is loading, show placeholder, than change placeholder to img
+    */
+    const article = document.createElement('article');
+    article.classList.add('card-wrapper');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name);
+    img.classList.add('card-image');
+
+    const h2 = document.createElement('h2');
+    h2.append(user.name);
+    h2.classList.add('username');
+
+    const p = document.createElement('p');
+    p.append(user.description);
+    p.classList.add('description');
+
+    article.append(img, h2, p);
+
+    return article;
+}
 
 
-/*
-Створити картинку і вставити її в div
-
-*/
-
-
-const img = document.createElement('img');
-img.setAttribute('src', './images/pict.jpg');
-img.classList.add('picture');
-div.append(img);
+root.append(...cardArray);
