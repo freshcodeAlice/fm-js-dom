d49e25f9107d12b1ec3e0a369bf547c7d13ea359
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-16 09:24:46 +0300

    Refactor

--- index.js
+++ index.js
@@ -8,26 +8,32 @@ function createCard(user) {
     2. Create image placeholder: first letter of name + random background-color
     3. When image is loading, show placeholder, than change placeholder to img
     */
-    const article = document.createElement('article');
-    article.classList.add('card-wrapper');
-
     const img = document.createElement('img');
     img.setAttribute('src', user.profilePicture);
     img.setAttribute('alt', user.name);
     img.classList.add('card-image');
 
-    const h2 = document.createElement('h2');
-    h2.append(user.name);
-    h2.classList.add('username');
+    const h2 = createElement('h2', {classNames: ['username']}, user.name);
+    const p = createElement('p', {classNames: ['desciption', 'super-pagraph']}, user.description);
 
-    const p = document.createElement('p');
-    p.append(user.description);
-    p.classList.add('description');
-
-    article.append(img, h2, p);
-
-    return article;
+    return createElement('article', {classNames: ['card-wrapper']}, img, h2, p);
 }
+
+/**
+ * 
+ * @param {String} type - тип елемента, що треба створити
+ * @param {Object} options 
+ * @param {String[]} options.classNames - список классів
+ * @param {Node} children - список дочірніх вузлів
+ */
+
+function createElement(type, {classNames}, ...children) {
+    const elem = document.createElement(type);
+    elem.classList.add(...classNames);
+    elem.append(...children);
+    return elem;
+}
+
 
 
 root.append(...cardArray);
