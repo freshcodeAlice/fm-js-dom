9fde29123f42fe668da0a284a3af3c97c7f05d90
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-19 10:57:37 +0300

    Div+input task resolve

--- index.js
+++ index.js
@@ -1,104 +1,28 @@
 /*
-
-1. Зробити інпут для вводу тексту
-2. Зробити кнопку, яка по натисненню текст з інпута перетворює на елемент списку.
-
-<input type="text" />
-<button>Click to add</buton>
-<ul>
-    <li>Зробити щось одне</li> <-- ось тут кнопка самознищення елементу списку
-    <li>Зробити щось інше</li>
-</ul>
-
-Задачка з *:
-до кожного li приєднати кнопку, за допомогою якої елемент зі списку можна видалити.
-
-Задачка з **:
-до кожного li доєднати другу кнопку, за натиснення якої можна відредагувати текст li
-Створювати новий input поряд з li, кнопка яка була для зміни 
-
-
-<li> Some text <button>Change text</button><button>X</button>
----> якщо натиснути на change text -->
-<li> Some text <input type="text"><button>Save</button></li>
+1. В html створити квадрат 50х50
+2. за натиснення під ним створити інпут для вводу тексту
+3. За ентером інпут зникає, а в квадраті відображається те, що було введено в інпут
 */
 
 
+const box = document.querySelector('#box');
 
-const root = document.getElementById('root');
-const form = document.getElementById('form');
-
-const state = [];
-
-form.addEventListener('submit', addItem);
-
-function addItem(event) {
-    event.preventDefault();
-    const { target: { textInput: { value } } } = event;
-    state.push(value);
-    addOneLi(value);
-}
-
-function addOneLi(value) {
-    const li = createItem(value);
-    root.append(li);
-}
-
-function updateView() {
-    const liArray = state.map(item => createItem(item));
-    root.append(...liArray);
-}
+box.addEventListener('click', boxClickHandler)
 
 
-function createItem(value) {
-    const li = document.createElement('li');
-    li.append(value, createButton({ 
-        btnText: 'Edit', 
-        handlers: { type: 'click', callback: editMode } }), 
-        createButton({
-            btnText: 'X',
-            handlers: {
-                type: 'click',
-                callback: deleteHandler
-            }
-        }));
-    li.classList.add('item');
-    li.dataset.id = state.length - 1;
-    return li;
-}
-
-
-/**
- * 
- * @param {String} options.btnText 
- * @param {Object} options.handlers
- */
-
-function createButton({ btnText, handlers: { type, callback } }) {
-    const btn = document.createElement('button');
-    btn.append(btnText);
-    btn.addEventListener(type, callback);
-    return btn;
-}
-
-function deleteHandler({ target }) {
-    //   const id = event.target.parentNode.dataset.id;
-    const { parentNode, parentNode: { dataset: { id } } } = target;
-    state.splice(id, 1);
-    parentNode.remove();
-}
-
-
-
-
-function editMode({ target: { parentNode } }) {
+function boxClickHandler(event) {
     const input = document.createElement('input');
-    input.addEventListener('blur', inputBlurHandler);
-    parentNode.append(input);
+    input.addEventListener('keydown', inputKeyHandler);
+    const parent = event.target.parentNode;
+    parent.append(input);
 }
 
-function inputBlurHandler({ target, target: { parentNode, value, parentNode: { dataset: { id } } } }) {
-    state[id] = value;
-    parentNode.firstChild.nodeValue = value;
-    target.remove();
-}
+function inputKeyHandler (event) {
+    if(event.keyCode === 13) {
+        const {target, target: {value, previousElementSibling}} = event;
+        // 2. Передали дані сусідньому елементу (боксу)
+        previousElementSibling.textContent = value;
+        //1. Знищили сам інпут
+        target.remove();
+    }
+};
