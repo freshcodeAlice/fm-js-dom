a13f07f002a8c6fc38abb10ad3b5b707b1c9d340
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-20 10:47:24 +0300

    setTimeout()

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-/*
-const regexp = /^Test$/; //шукаємо слово "Тест"
-/^[A-Za-z]{2,5}$/ //шукаємо слово з 2-5 літер (великих чи маленьких)
-/^[A-Za-z0-9\_\.]+\@[A-Za-z0-9]+\.[A-Za-z0-9]+$/ // email
+const id = setTimeout(func, 10000);
+console.log(id);
 
+function func() {
+    console.log(`2+2=${2+2}`);
+}
 
-*/
-const testString = 'sunnyday23@mail.com';
-const failTestString = '434sdf';
-
-const str1 = 'hello';
-const str2 = new String('hello');
-
-//RegExp
-
-const reg1 = /^[A-Za-z0-9\_\.]+\@[A-Za-z0-9]+\.[A-Za-z0-9]+$/mi;
-console.dir(reg1);
-const reg2 = new RegExp('^[A-Za-z]{2,5}$');
-console.log(reg2);
+clearTimeout(id);
